263d0fd0868da1fc2b8f610e8b82e50d678be07b
Saar Pardo <Saarpardo84@gmail.com>
2025-07-29 16:21:22 +0300

    290725

--- Udemy/MySQL Bootcamp/Section15 - VIEWS & MODES.sql
+++ Udemy/MySQL Bootcamp/Section15 - VIEWS & MODES.sql
@@ -33,5 +33,36 @@ JOIN reviewers ON reviewers.id = reviews.reviewer_id;
 -- DROP VIEW full_revies....
 
 
+-- HAVING = FILTERING THE GROUP BY
+
+SELECT title,
+    AVG(rating),
+    COUNT(rating)
+FROM full_reviews
+GROUP BY title HAVING COUNT(rating)>2;
 
 
+-- WITH ROLLUP = NEW LINE WITH summary stats for ENTIRE HIERARCHY
+
+SELECT title,
+    AVG(rating), 
+    COUNT(rating)
+FROM full_reviews
+GROUP BY title WITH ROLLUP;
+
+SELECT genre,released_year,
+    AVG(rating) 
+FROM full_reviews
+GROUP BY genre,released_year WITH ROLLUP;
+
+
+-- MODES
+
+SELECT @@GLOBAL.sql_mode;
+SELECT @@SESSION.sql_mode;
+# SET SESSION sql_mode = 'IGNORE_SPACE,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,
+#                       ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'
+                        
+# the mosr important mode:
+# STRICT_TRANS_TABLES - if i turn it off i can insert 'hi' as rate
+#SET SESSION sql_mode = '';
