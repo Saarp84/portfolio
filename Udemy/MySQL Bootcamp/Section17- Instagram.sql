e8c83bf11a536902096a924bbe37b716276d3bbc
Saar Pardo <Saarpardo84@gmail.com>
2025-08-02 18:48:02 +0300

    020825

--- Udemy/MySQL Bootcamp/Section17- Instagram.sql
+++ Udemy/MySQL Bootcamp/Section17- Instagram.sql
@@ -91,4 +91,18 @@ CREATE TABLE follows(
 -- INSERT INTO follows (follower_id,followee_id) VALUE (1,2)
 
 # Hashtags
+CREATE TABLE tags(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    tag_name VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags(
+    photo_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
+
 
